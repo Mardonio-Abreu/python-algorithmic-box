852a90edade6f57061927cc5b2a133694e6d8733
Mardonio-Abreu <mardonio.abreu@protonmail.com>
2023-03-22 13:23:38 -0600

    fixed, I was using string type instead of integer type

--- maximunPairwiseProduct.py
+++ maximunPairwiseProduct.py
@@ -1,12 +1,22 @@
+import time
+
 #print ("Enter the size")
 size = input()
 
 #print ("Enter the numbers")
 numbers = input()
-numberArray = numbers.split()
+stringArray = numbers.split()
+
+numberArray = []
+
+for i in stringArray:
+    numberArray.append(int(i))
+    
 
 numberArray.sort()
 numberArray.reverse()
 
 print(int(numberArray[0])*int(numberArray[1]))
+
+print ()
 
